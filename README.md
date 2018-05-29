12965c6107adc5eae391fd0eb17ca79c5c394e88
Michael Santos <michael.santos@gmail.com>
2018-05-29 08:21:05 -0400

    xep0363: HTTP Upload
    Example code for downloading and decrypting the AES256GCM files created
    by XEP0363.
    
    Warning: this code decrypts whole files. Quick check of the man page
    suggests the erlang crypto interface doesn't support streaming aes_gcm yet.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# xep0363_crypt
-XEP-0363 (XMPP HTTP File Upload)  file encryption/decryption
+xep0363_crypt
+=============
+
+XEP-0363 (HTTP File Upload) file encryption/decryption
+
+Build
+-----
+
+    $ rebar3 compile
+
---
+++ src/xep0363_crypt.erl
@@ -0,0 +1,127 @@
+-module(xep0363_crypt).
+
+%% API exports
+-export([uri/1,
+         fetch/1,
+         decrypt/2,
+         decrypt_file/2,
+         encrypt/1, encrypt/2,
+         encrypt_file/1, encrypt_file/2,
+         iveckey/1]).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+-spec uri(string() | binary())
+  -> {ok, binary(), {aesgcm, binary()} | plaintext} | {error, atom()}.
+uri(URI) when is_list(URI) ->
+  uri(list_to_binary(URI));
+uri(<<"aesgcm://", URI/binary>>) ->
+  case binary:split(URI, <<"#">>) of
+    [Path, Key] ->
+      {ok, list_to_binary(["https://", Path]), {aesgcm, Key}};
+    _ ->
+      {error, invalid}
+  end;
+uri(<<"https://", _/binary>> = URI) ->
+  {ok, URI, plaintext};
+uri(_) ->
+  {error, unsupported}.
+
+-spec fetch(string() | binary()) -> {ok, binary(), binary()} | {error, term()}.
+fetch(URI0) ->
+  case uri(URI0) of
+    {error, _} = Error ->
+      Error;
+    {ok, URI, Key} ->
+      case httpc:request(["https://", URI]) of
+        {ok, {{_,200,_}, _Headers, Body}} ->
+          {ok, Key, list_to_binary(Body)};
+        {error, _} = Error ->
+          Error;
+        Unknown ->
+          {error, Unknown}
+      end
+  end.
+
+-spec decrypt_file(file:name_all(), binary() | {aesgcm, binary()} | plaintext)
+  -> {ok, binary()} | {error, file:posix() | badarg | terminated | system_limit}.
+decrypt_file(File, KeyHex) ->
+  case file:read_file(File) of
+    {ok, CipherText} ->
+      case decrypt(CipherText, KeyHex) of
+        error ->
+          {error, decrypt_failed};
+        PlainText when is_binary(PlainText) ->
+          {ok, PlainText}
+      end;
+    {error, _} = Error ->
+      Error
+  end.
+
+-spec iveckey(<<_:768>>) -> {<<_:128>> | <<_:96>>, <<_:256>>}.
+iveckey(<<_:96/bytes>> = Hex) ->
+  Bytes = << <<(binary_to_integer(N, 16))>> || <<N:2/bytes>> <= Hex >>,
+  case Bytes of
+    % old format
+    <<Ivec:16/bytes, Key:32/bytes>> ->
+      {Ivec, Key};
+    % new format
+    <<Ivec:12/bytes, Key:32/bytes>> ->
+      {Ivec, Key}
+  end.
+
+-spec decrypt(iodata(), binary() | {aesgcm, binary()} | plaintext)
+  -> binary() | error.
+decrypt(CipherText, Key) when is_list(CipherText) ->
+  decrypt(iolist_to_binary(CipherText), Key);
+decrypt(PlainText, plaintext) ->
+  PlainText;
+decrypt(CipherText, {aesgcm, KeyHex}) ->
+  {Ivec, KeyBytes} = iveckey(KeyHex),
+  CipherLen = byte_size(CipherText) - 16,
+  <<CipherData:CipherLen/bytes, CipherTag:16/bytes>> = CipherText,
+  crypto:block_decrypt(aes_gcm, KeyBytes, Ivec,
+                       {<<>>, CipherData, CipherTag});
+decrypt(CipherText, Key) when is_binary(Key) ->
+  decrypt(CipherText, {aesgcm, Key}).
+
+-spec encrypt_file(file:filename_all())
+  -> {ok, {iodata(), binary()}} | {error, file:posix() | badarg | terminated | system_limit}.
+encrypt_file(File) ->
+  encrypt_file(File, {aesgcm, 16}).
+
+-spec encrypt_file(file:filename_all(), {aesgcm, 12 | 16} | plaintext)
+  -> {ok, {iodata(), binary()}} | {error, file:posix() | badarg | terminated | system_limit}.
+encrypt_file(File, {aesgcm, _} = IVLen) ->
+  case file:read_file(File) of
+    {ok, PlainText} ->
+      {ok, encrypt(PlainText, IVLen)};
+    {error, _} = Error ->
+      Error
+  end.
+
+-spec encrypt(binary()) -> {iodata(), binary()}.
+encrypt(PlainText) ->
+  encrypt(PlainText, {aesgcm, 16}).
+
+-spec encrypt(binary(), {aesgcm, 12 | 16} | plaintext) -> {iodata(), binary() | plaintext}.
+encrypt(PlainText, plaintext) ->
+  {PlainText, plaintext};
+encrypt(PlainText, {aesgcm, IVLen}) ->
+  Key = crypto:strong_rand_bytes(32),
+  IVec = crypto:strong_rand_bytes(IVLen),
+  {CipherText, CipherTag} = crypto:block_encrypt(
+                              aes_gcm,
+                              Key,
+                              IVec,
+                              {<<>>, PlainText, 16}
+                             ),
+  {[CipherText, CipherTag], to_hex(<<IVec/binary, Key/binary>>)}.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+to_hex(Bin) ->
+  << <<(integer_to_binary(X,16))/binary>> || <<X:4>> <= Bin >>.
